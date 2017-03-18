daa08d730ad868b316d0c1f8648f136de21cfd8b
Liyuezhou <1933564949@qq.com>
2017-03-18 12:52:17 +0800

    倔强的登录页

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,8 +1,26 @@
 
  angular.module('App')
-	.controller("denglu",["$scope",function($socpe){
-		$socpe.longin=function(){
-			alert(123)
+	.controller("denglu",["$scope","$http","$state","$timeout",function($scope,$http,$state,$timeout){
+		$scope.show = false;
+		$scope.login=function(){
+			$http({
+				url:"http://47.88.16.225:402/users/login",
+				method:"post",
+				data:{
+					username:$scope.username,
+					password:$scope.passwords
+				}
+			}).then(function(data){
+				$scope.showTitle = "登录成功"
+				$scope.show = true;
+				$state.go("contrain")		
+			},function(data){
+				$scope.showTitle = "请输入正确的手机号"
+				$scope.show = true;
+				$timeout(function(){
+				    $scope.show = false;
+				},600);
+			})
 		}
 	}])
 			
--- app/scripts/controllers/scripts.js
+++ app/scripts/controllers/scripts.js
@@ -1,31 +1,61 @@
-
-jQuery(document).ready(function() {
-
-    $('.page-container form').submit(function(){
-        var username = $(this).find('.username').val();
-        var password = $(this).find('.password').val();
-        if(username == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '27px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.username').focus();
-            });
-            return false;
-        }
-        if(password == '') {
-            $(this).find('.error').fadeOut('fast', function(){
-                $(this).css('top', '96px');
-            });
-            $(this).find('.error').fadeIn('fast', function(){
-                $(this).parent().find('.password').focus();
-            });
-            return false;
-        }
-    });
-
-    $('.page-container form .username, .page-container form .password').keyup(function(){
-        $(this).parent().find('.error').fadeOut('fast');
-    });
-
-});
+var oInt=document.getElementsByTagName("input");
+var oNext=document.getElementsByClassName("next")[0];
+var oMsg=document.getElementsByClassName("msg");
+var username=document.getElementsByClassName("username")[0];
+username.onkeyup = function(){
+ this.value = this.value.replace(/[^0-9]/, '');
+};
+	for (var i=0;i<oInt.length;i++) {
+		oInt[i].oninput=function(){
+			if(oInt[0].value==""||oInt[1].value==""){
+				oNext.style.color="#ED8B64"
+			}else{
+				oNext.style.color="#FFFFFF"
+			}
+		}
+	}
+	
+	//信息注册验证
+	//密码验证
+	var passWordText=/^[a-zA-Z]\w{5,17}|\d{5,17}$/;
+	oInt[1].onfocus=function(){
+		if(this.value=="")
+		this.placeholder="";
+		oMsg[1].style.display="none";
+	}
+	oInt[1].onblur=function(){
+		if(this.value){		
+					if(passWordText.test(this.value)){
+					}
+					else{
+						oMsg[1].style.display="block";
+						this.value=""
+					}
+		}
+		else{
+			this.placeholder="请输入登录密码";
+		}
+	}
+	//手机号验证
+	oInt[0].onfocus=function(){
+		if(this.value==""){
+			this.placeholder="";
+			oMsg[0].style.display="none";
+		}
+		
+	}
+	var phoneText=/^1(3\d|47|5[0-35-9]|7[0167]|8\d)\d{8}$/;
+	oInt[0].onblur=function(){
+		if(this.value){
+			
+					if(phoneText.test(this.value)){
+					}
+					else{
+						oMsg[0].style.display="block";
+						this.value=""
+					}
+		}
+		else{
+			this.placeholder="请输入手机号";
+		}
+	}
