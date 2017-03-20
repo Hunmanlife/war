72c7e5ff6ef76d6193f3c894e4f211afe364c7f6
Liyuezhou <1933564949@qq.com>
2017-03-20 11:05:34 +0800

    陈总，王八蛋

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,24 +14,19 @@ angular
 	$stateProvider.state('denglu',{
 		url:"/login",
 		templateUrl:"views/login.html"
-	});
-	
-	$stateProvider.state('contrain',{
+	})
+	.state('contrain',{
 		url:"/contrain",
 		templateUrl:"views/contrain.html"
-	});
-	
-	
-	$stateProvider.state('contrain.todayNews',{
+	})
+	.state('contrain.todayNews',{
 		url:"/todayNews",
 		templateUrl:"views/todayNews.html"	
-	});
-	$stateProvider.state('contrain.todayNews.rizhixq',{
+	})
+	.state('contrain.todayNews.rizhixq',{
 		url:"/rizhixq",
 		templateUrl:"views/rizhixq.html"	
 	});
-	
-	
 	$urlRouterProvider.when("","/login")
 }])
 
--- app/scripts/controllers/rizhi.js
+++ app/scripts/controllers/rizhi.js
@@ -1,28 +1,28 @@
    angular.module('App')
 	.controller("contrain",["$scope","$http","$state","$timeout",function($scope,$http,$state,$timeout){
 		$scope.tiao = function(){
-			$state.go("contrain.todayNews")	;
-//			$(function(){
-//				$(".rizhi").css("background","#033F4B")
-//			})
-			
+			$state.go("contrain.todayNews")	;			
 		}
 		
 	}])
-	.controller("todayNews",function($scope){
-//		$scope.gao=function(){
-				$(function(){
-					$("#todayNews>ul>li").click(function(){
-						$(this).css("background","#099DBD").siblings().css("background","#033F4B")
-					})
-				})
-//		}
+	.controller("rizhixq",function($scope){
+		$(function(){
+			$("#todayNews>ul>li").eq(0).css("background","#31708F");
+			$(".todayInfo").eq(0).css("display","block");
+			$("#todayNews>ul>li").click(function(){
+				$(this).css("background","#31708F").siblings().css("background","#033F4B");
+				$(".todayInfo").css("display","none");
+				$(".todayInfo").eq($(this).index()).css("display","block");
+			})
+			
+			$(".todayInfo>p").eq(0).css("background","#1B6D85");
+			$(".todayInfo>ul").eq(0).css("display","block");
+			$(".todayInfo>p").click(function(){
+				$(this).css("background","#1B6D85").siblings("p").css("background","#0D648E");
+				$(".todayInfo>ul").css("display","none");
+				$(".todayInfo>ul").eq($(this).index()).css("display","block")
+			})
+		})
 	})
-//	$(function(){
-//		$("#todayNews>ul>li").click(function(){
-//	//		$(this).className("active").siblings().remove("class")
-//			alert("123")
-//		})
-//	})
 	
 			
